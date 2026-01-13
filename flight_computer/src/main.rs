4a99f9c8f00ed348b55e6533c8ff5ffb70b188c8
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2026-01-12 19:03:15 -0500

    Add Basic Flight Computer Camera Logic

--- flight_computer/src/main.rs
+++ flight_computer/src/main.rs
@@ -1,3 +1,29 @@
+use nokhwa::{
+    CallbackCamera, Camera,
+    pixel_format::{LumaFormat, RgbFormat, YuyvFormat},
+    query,
+    utils::{CameraIndex, RequestedFormat},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let cameras = query(nokhwa::utils::ApiBackend::Auto).unwrap();
+    println!("Cameras: {:#?}", cameras);
+
+    // first camera in system
+    let index = CameraIndex::Index(3);
+    // request the absolute highest resolution CameraFormat that can be decoded to RGB.
+    let requested = RequestedFormat::new::<LumaFormat>(
+        nokhwa::utils::RequestedFormatType::AbsoluteHighestFrameRate,
+    );
+    // make the camera
+    let mut camera = CallbackCamera::new(index, requested, move |buffer| {
+        let decoded = buffer.decode_image::<LumaFormat>().unwrap();
+        println!("Decoded Frame of {}", decoded.len());
+        decoded.save("./test.png").unwrap();
+    })
+    .unwrap();
+
+    camera.open_stream().unwrap();
+
+    loop {}
 }
